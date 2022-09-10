c0a59dd7cfd73c83542165cd9fa30e1e7593377b
Ehsanul-Karim-Pappu <ehsan.pappu.99@gmail.com>
2022-09-10 16:30:08 +0600

    Data Biniding - String Interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
-  selector: '.app-server',
+  selector: 'app-server',
   templateUrl: './server.component.html',
   styleUrls: ['./server.component.css']
 })
-export class ServerComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ServerComponent {
+  serverId = 10;
+  serverStatus = 'offline';
 }
