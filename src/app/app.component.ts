7eef74cfaf6885c6fec57e4dec247567532c19fd
Ehsanul-Karim-Pappu <ehsan.pappu.99@gmail.com>
2022-09-11 10:01:04 +0600

    property binding and added bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-Udemy';
+  allowNewServer = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 3000)
+  }
 }
