2c06fe3d6cbd316a20401e3302023e0a0d13b29f
Ehsanul-Karim-Pappu <ehsan.pappu.99@gmail.com>
2022-09-18 21:25:35 +0600

    Added pause funtionality instead of stop button

--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -7,7 +7,7 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 })
 export class GameControlComponent implements OnInit {
   @Output() intervalFired = new EventEmitter<number>();
-  @Output() clearNumArray = new EventEmitter<number>();
+  // @Output() clearNumArray = new EventEmitter<number>();
   intervalId: ReturnType<typeof setInterval> = setInterval(() => {});
   // in tsconfig.app.json add the attribute "node" to the types field.
   // intervalId: NodeJS.Timer = setInterval(() => {});
@@ -20,14 +20,14 @@ export class GameControlComponent implements OnInit {
 
   onClickStart() {
       this.intervalId = setInterval(() => {
-        this.intervalFired.emit(this.num++);
+        this.intervalFired.emit(++this.num);
       }, 1000);
   }
 
   onClickStop() {
     clearInterval(this.intervalId);
-    this.clearNumArray.emit();
-    this.num = 0;
+    // this.clearNumArray.emit();
+    // this.num = 0;
   }
 
 }
