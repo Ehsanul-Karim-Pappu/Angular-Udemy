d6c31e5e93b909d9a1ae05a3a27dd15b3945b42f
Ehsanul-Karim-Pappu <ehsan.pappu.99@gmail.com>
2022-09-26 23:24:31 +0600

    Binding to Directive Properties 2

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -5,7 +5,7 @@ import {Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Rendere
 })
 export class BetterHighlightDirective implements OnInit{
   @Input() defaultColor: string = '#9ffffc';
-  @Input() highlightColor: string = '#bfff9f';
+  @Input('appBetterHighlight') highlightColor: string = '#bfff9f';
   @HostBinding('style.backgroundColor') backgroundColor!: string;
 
   constructor(private elref: ElementRef, private renderer: Renderer2) { }
